c1d0bd8bad42b35e379253d896ad3f52be76f70f
burin2301 <burin2301@gmail.com>
2023-03-09 23:52:30 -0500

    Completed:     * Main game completed
    Pending:
        * Catch for the user pressing something that is not 1, 2 or 3
        * Lives count of the game and looping the game until 5 losts

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+//We create the variables, also i set up a prompt to get the player choice
 
 let player = parseInt(prompt("Choose 1 for Rock, 2 for Paper and 3 for Scissors"))
 let pc 
 let result
 
+let playerLoses = 0
+let pcLoses = 0
+
+//I create a function where the PC with calculations and random numbers gets a number and the switch translates it into rock paper or scissors
 
 function pcSelection(){
    let number = Math.floor(Math.random() * (3) + 1 )
@@ -16,6 +21,7 @@ function pcSelection(){
     }
 } 
 
+//Here I translates the choice of the player into rock, paper or scissors
 
 function playerSelection(){
     
@@ -31,29 +37,50 @@ function playerSelection(){
         
 }
 
+//Here, i make the functions asign the choice into a variable
+
 pc = pcSelection()
 player = playerSelection()
 
+
+
+
+//Here i compare both choices, i add the variables and create the switch's cases knowing the output of the sum of variables
+
 function comparison(){
     switch(pc+player){
+
         case "PaperRock":
         case "ScissorsPaper":
         case "RockScissors":
-            return result = "The PC has Won!!!!"
+            result = "The PC has Won!!!!, "+pc+" beats "+player+"!!!";
+            return 
+
         case "RockPaper":
         case "PaperScissors":
         case "ScissorsRock":
-            return result = "You have Won!!!!"
+            
+            result = "You have Won!!!!, "+player+" beats "+pc+"!!!";
+            return 
+            
         case "ScissorsScissors":
         case "PaperPaper":
         case "RockRock":
-            return result = "It is a Draw!!!!"
+            result = "Oh, "+player+" and "+pc+" makes a Draw!!!!";
+            return 
 
     }
 }
+
+
+//Finally, i add the result of the switch's cases to the result variable
+
 result = comparison()
+pcLoses = comparison()
+playerLoses = comparison()
 
-
-console.log(player)
-console.log(pc)
+//Prints of the round
+console.log("The player chooses " + player)
+console.log("The pc chooses " + pc)
 console.log(result)
+//console.log("Pc has lost: " + pcLoses + "times, and the Player has lost: " + playerLoses + "times!" )
